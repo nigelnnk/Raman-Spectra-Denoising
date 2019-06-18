a27c2e39fd150440aadf022d40739168943c6bda
Nigel Neo <17807884+nigelnnk@users.noreply.github.com>
2019-06-18 17:27:01 +0800

    New Method for Generating Random Spectra

--- generate_spectrum.py
+++ generate_spectrum.py
@@ -1,4 +1,5 @@
 import math
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 from numba import *
@@ -36,12 +37,25 @@ def populate(x, list_of_signals):
     return i
 
 
+def generate_random(wavenumbers):
+    n = random.randint(2, 7)
+    spectrum = np.zeros(wavenumbers.size)
+    for i in range(n):
+        p = random.choice(wavenumbers)
+        w = random.randint(5, 30)
+        h = random.randint(1, 5)
+        print("{}\t{}\t{}".format(p, w, h))
+        spectrum += lorentzian(wavenumbers, p, w, h)
+    return spectrum
+
+
 def main():
     """
     For debugging purposes, to print out certain functions and see how good they are
     :return: A plot of the signal, noisy spectrum, and some functions
     """
     x = np.load("data/spectrum_clean.npz")["wavenumbers"]
+    x = np.flip(x)
     signal = populate(x, LSIGNAL)
     # wavenumbers, signal = sl.read_spectrum("data/4.csv")
     # _, noise = sl.read_spectrum("data/23.csv")
@@ -58,26 +72,25 @@ def main():
     ax[0, 1].plot(x, noise)
     ax[0, 1].set_title("Noisy Spectrum")
     convolved = sf.iter_convo_filter(noise, 5)
-    ax[0, 2].plot(x, convolved)
-    ax[0, 2].set_title("Iterative SG convolution")
+    ax[1, 0].plot(x, convolved)
+    ax[1, 0].set_title("Iterative Convolution Smoothing")
 
     ds, cs = pf.get_corrected_spectrum(noise, 5, 53)
-    ax[1, 0].plot(x[:-1], ds, color='C1')
-    ax[1, 0].set_title("Differentiated")
-    ax[1, 1].plot(x[:-1], cs, color='C1')
-    ax[1, 1].set_title("Corrected")
+    ax[0, 2].plot(x[:-1], ds, color='C1')
+    ax[0, 2].set_title("Differentiated")
+    ax[1, 2].plot(x[:-1], cs, color='C1')
+    ax[1, 2].set_title("Corrected")
 
     # new_x, new_y = pf.detect_peaks(cs, x[:-1])
     # print(new_x)
     # print(new_y)
     # ax[1, 1].plot(new_x, new_y, color='b', marker="x", markersize=6)
 
-    ax[1, 2].plot(x[:-1], -np.diff(signal))
-    ax[1, 2].set_title("Noise")
+    ax[1, 1].plot(x, sf.convo_filter_n(noise, 5, 20))
+    ax[1, 1].set_title("Convolution filter (20)")
 
     plt.show()
 
 
 if __name__ == "__main__":
     main()
-
